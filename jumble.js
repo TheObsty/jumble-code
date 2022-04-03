d4883dc6d13f7cf5ffa5ca0408ad3aa262bf2ee9
TheObsty <102871545+TheObsty@users.noreply.github.com>
2022-04-04 00:36:28 +0530

    Update jumble.js

--- jumble.js
+++ jumble.js
@@ -1,39 +1,30 @@
-const initial = document.querySelector('.start');
-const guess = document.querySelector('input');
-const div = document.querySelector('h3');
-const btn = document.querySelector('.btn');
-const btn2 = document.querySelector('.btn2');
+const greetMessage = document.querySelector('.title')
+const messageLabel = document.querySelector('.start');
+const guessWord = document.querySelector('input');
+const scoreHeader = document.querySelector('h3');
+const startButton = document.querySelector('.btn');
+const stopButton = document.querySelector('.btn2');
 const score = document.querySelector('.score1');
 score.innerHTML = 0;
-let sc = 0;
+
 let game = false;
-let newV = "";
-let randomV = "";
+let highScore = localStorage.getItem('highScore', 0);
+let defaultScore = 0;
+let newWord = "";
+let randomWord = "";
+stopButton.addEventListener('click', endGame);
+startButton.addEventListener('click', startGame);
 
-const words = ['apple',
-    'ball',
-    'cat',
-    'dog',
-    'elephant',
-    'frog',
-    'guitar',
-    'house',
-    'ice',
-    'jacket',
-    'karen',
-    'lemon',
-    'mango',
-    'naughty',
-    'oops'];
+const wordBank = ['apple', 'ball', 'cat', 'dog'];
 
-const createwords = () => {
-    let number = Math.floor(Math.random() * words.length);
-    let random = words[number];
+const createWord = () => {
+    let number = Math.floor(Math.random() * wordBank.length);
+    let random = wordBank[number];
     console.log(random.split(""));
     return random;
 }
 
-const jumbleword = (el) => {
+const jumbleWord = (el) => {
     for (let i = el.length - 1; i >= 0; i--) {
         let temp = el[i];
         let j = Math.floor(Math.random() * (i + 1));
@@ -43,77 +34,80 @@ const jumbleword = (el) => {
     return el.join("");
 }
 
-const gamemenu = () => {
-    btn2.style.display = "none";
+function gameMenu() {
+    greetMessage.style.display = "block";
+    stopButton.style.display = "none";
+    guessWord.style.display = "none";
+    messageLabel.style.display = "none";
 }
 
-btn.addEventListener('click', function () {
+function gameCheck() {
+    if (game == true) {
+        greetMessage.style.display = "none";
+        guessWord.style.display = "block";
+        scoreHeader.style.display = 'block';
+        score.style.display = 'block';
+        stopButton.style.display = "block";
+    } else {
+        greetMessage.style.display = "none";
+        guessWord.style.display = "none";
+        if (guessWord.value == newWord) {
+            messageLabel.innerHTML = `Awesome ! It's Correct.\nIt is ${newWord}`;
+            startButton.textContent = "Next word";
+            defaultScore += 1;
+            console.log(defaultScore);
+            score.innerHTML = defaultScore;
+            guessWord.value = "";
+        } else {
+            messageLabel.innerHTML = `Better luck next time, buddy!\nThe word was ${newWord}`;
+            startButton.textContent = "Try again ?";
+            console.log(defaultScore);
+            gameHighScore();
+        }
+    }
+}
+
+function startGame() {
     if (!game) {
         game = true;
-
-        btn.textContent = "Guess !!";
-        guess.style.display = "block";
-        div.style.display = 'block';
-        score.style.display = 'block';
-        btn2.style.display = "block";
-        newV = createwords();
-        randomV = jumbleword(newV.split(""));
-        console.log(randomV);
-        initial.style.display = "block";
-        initial.innerHTML = 'Guess the word \"' + randomV + '\"';
-
-    } else {
-
-        let inputword = guess.value;
-        if (inputword == newV) {
-            console.log('Correct');
-            game = false;
-            initial.style.display = "block";
-            initial.innerHTML = `Awesome ! It's Correct.\nIt is ${newV}`;
-            sc += 1;
-            score.innerHTML = sc;
-            console.log(sc);
-            guess.style.display = "none";
-            // guess.classList.toggle('hidden');
-            btn.innerHTML = "Next word!";
-            guess.value = "";
-
-        } else {
-            console.log('Incorrect');
-            game = false;
-            initial.style.display = "block";
-            initial.innerHTML = `Better luck next time, buddy!\nThe word was ${newV}`;
-            if (sc <= 0) {
-                sc = 0;
-                score.innerHTML = sc;
-                console.log(sc);
-            }
-            else {
-                sc -= 1
-                score.innerHTML = sc;
-                console.log(sc);
-            }
-            btn.innerHTML = "Play again !?"
-            guess.style.display = "none";
-            // guess.classList.toggle('hidden');
-            guess.value = "";
-        }
-
+        startButton.textContent = "Guess !!";
+        gameCheck();
+        newWord = createWord();
+        randomWord = jumbleWord(newWord.split(""));
+        console.log(randomWord);
+        messageLabel.style.display = "block";
+        messageLabel.innerHTML = 'Guess the word : \"' + randomWord + '\"'
     }
-})
+    else if (guessWord.value == newWord) {
+        game = false;
+        gameCheck();
+    } else {
+        game = false;
+        console.log("Wrong")
+        console.log(defaultScore);
+        gameCheck();
+        gameHighScore();
+    }
+}
 
-btn2.addEventListener('click', function () {
-    // window.location.reload();
+function endGame() {
     game = false;
-    div.style.display = 'block';
-    score.style.display = 'block';
-    guess.style.display = 'none';
-    initial.style.display = 'none';
-    div.innerHTML = "High score"
-    const hisc = sc;
-    console.log(hisc);
-    score.innerHTML = `${hisc}`;
-    gamemenu();
-})
+    gameHighScore();
+    gameMenu();
+}
 
-window.onload = gamemenu;
+function gameHighScore() {
+    if (highScore !== null) {
+        if (defaultScore > highScore) {
+            localStorage.setItem("highScore", defaultScore);
+            scoreHeader.innerHTML = "High score"
+            score.innerHTML = highScore;
+        } else {
+            localStorage.setItem("highScore", defaultScore);
+        }
+    }
+}
+
+window.onload = function () {
+    gameMenu();
+}   
